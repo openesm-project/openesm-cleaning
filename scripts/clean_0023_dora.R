2adf8a0bb91eef5a8b1daec577beedf1165b9f9d
Matze Kloft <83857345+matthiaskloft@users.noreply.github.com>
2025-04-11 16:42:51 +0200

    update: 0023_dora, data cleaning, metadata coding

---
+++ scripts/clean_0023_dora.R
@@ -0,0 +1,82 @@
+# Packages ----------------------------------------------------------------
+library(tidyverse)
+library(here)
+library(googlesheets4)
+library(jsonlite)
+library(osfr)
+source(here("scripts", "functions_data.R"))
+
+
+
+# Data --------------------------------------------------------------------
+if(!file.exists(here("data", "raw", "0023_dora_ts_raw.csv"))){
+  osf_retrieve_file("https://osf.io/chb5m") |>
+    osf_download(path = here("data", "raw"))
+
+
+  # rename data
+  file_name <- osf_retrieve_file("https://osf.io/chb5m") |> pull(name)
+  file.rename(here("data", "raw", file_name), here("data", "raw", "0023_dora_ts_raw.csv"))
+}
+
+df <- read.csv(here("data", "raw", "0023_dora_ts_raw.csv"))
+
+
+
+# Cleaning ----------------------------------------------------------------
+
+#* Column Names -----------------------------------------------------------
+df <- df |>
+  janitor::clean_names() |>
+  dplyr::rename(id = pid,
+                day = study_day,
+                beep = version)
+
+
+#* Misc -------------------------------------------------------------------
+# Remove unnecessary column
+df$x <- NULL
+
+
+# split off demographic data to separate file
+df_demographics <- df |>
+  group_by(id) |>
+  distinct(id, age, gender, ddq_typ_drinks)
+
+# save demographic data
+write_tsv(df_demographics, here("data", "clean", "0023_dora_static.tsv"))
+
+# remove demographic columns from main data
+df <- df |>
+  select(-c(age, gender, ddq_typ_drinks))
+
+
+# Check requirements ------------------------------------------------------
+# if check_data runs without messages, the data are clean
+# and should be saved as a .tsv file
+check_results <- check_data(df)
+check_results
+
+# if it returns "Data are clean.", save the data
+# Enter data set ID here
+if(check_results == "Data are clean."){
+  write_tsv(df, here("data", "clean", "0023_dora_ts.tsv"))
+}
+
+
+# Create metadata ---------------------------------------------------------
+metadata_url <- "https://docs.google.com/spreadsheets/d/1ALGCq_jN6I4dcjWYQ_LQe9o52DGJItwdu9fCkwOh6fg/edit?pli=1&gid=0#gid=0"
+meta_data <- read_sheet(metadata_url)
+
+
+# Enter dataset ID here
+sheet_url <- meta_data |>
+  filter(id == "0023") |>
+  pull("Coding File URL")
+
+variable_data <- read_sheet(sheet_url)
+
+meta_json <- create_metadata_json("0023") |>
+  toJSON(pretty = TRUE, auto_unbox = TRUE)
+
+write(meta_json, here("data", "metadata", "0023_dora_metadata.json"))
