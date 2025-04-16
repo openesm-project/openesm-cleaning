f527b73cd872be008c18fce7bea3de68aa038f97
Matze Kloft <83857345+matthiaskloft@users.noreply.github.com>
2025-04-16 18:25:58 +0200

    0031_koval

---
+++ scripts/clean_0031_koval.R
@@ -0,0 +1,72 @@
+# Packages ----------------------------------------------------------------
+library(tidyverse)
+library(here)
+library(googlesheets4)
+library(jsonlite)
+library(osfr)
+source(here("scripts", "functions_data.R"))
+
+
+
+# Data --------------------------------------------------------------------
+if(!file.exists(here("data", "raw", "0031_koval_ts_raw.csv"))){
+  osf_retrieve_file("https://osf.io/7sa9k") |>
+    osf_download(path = here("data", "raw"))
+
+
+  # rename data
+  file_name <- osf_retrieve_file("https://osf.io/7sa9k") |> pull(name)
+  file.rename(here("data", "raw", file_name), here("data", "raw", "0031_koval_ts_raw.csv"))
+}
+
+df <- read_delim(here("data", "raw", "0031_koval_ts_raw.csv"),
+                 delim = ";", escape_double = FALSE, na = "empty",
+                 trim_ws = TRUE)
+
+
+
+# Cleaning ----------------------------------------------------------------
+
+#* Column Names -----------------------------------------------------------
+df <- df |>
+  janitor::clean_names() |>
+  dplyr::rename(id = pid,
+                day = unit,
+                beep = occasion,
+                positive_affect = pa,
+                negative_affect = na)
+
+#* Misc -------------------------------------------------------------------
+
+
+
+
+# Check requirements ------------------------------------------------------
+# if check_data runs without messages, the data are clean
+# and should be saved as a .tsv file
+check_results <- check_data(df)
+check_results
+
+# if it returns "Data are clean.", save the data
+# Enter data set ID here
+if(check_results == "Data are clean."){
+  write_tsv(df, here("data", "clean", "0031_koval_ts.tsv"))
+}
+
+
+# Create metadata ---------------------------------------------------------
+metadata_url <- "https://docs.google.com/spreadsheets/d/19c-wGlBzhn6wjNqYP0nCaGPNY1aWUa149B2M5Bkx7xY/edit?gid=0#gid=0"
+meta_data <- read_sheet(metadata_url)
+
+
+# Enter dataset ID here
+sheet_url <- meta_data |>
+  filter(id == "0031") |>
+  pull("Coding File URL")
+
+variable_data <- read_sheet(sheet_url)
+
+meta_json <- create_metadata_json("0031") |>
+  toJSON(pretty = TRUE, auto_unbox = TRUE)
+
+write(meta_json, here("data", "metadata", "0031_koval_metadata.json"))
