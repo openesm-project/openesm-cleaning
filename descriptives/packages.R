4eee8a285101244217c3e7c16580320dcc29ff57
Bjoern Siepe <bjoernsiepe@gmail.com>
2026-05-26 05:56:02 +0200

    update pipeline to only include summary stats

--- descriptives/packages.R
+++ descriptives/packages.R
@@ -1,8 +1,4 @@
-# descriptives/packages.R
-# Install R dependencies for compute_descriptives.R.
-# Used by GitHub Actions to populate the package cache.
-
-pkgs <- c("dplyr", "readr", "e1071", "diptest", "jsonlite", "here")
+pkgs <- c("dplyr", "readr", "e1071", "jsonlite", "here")
 to_install <- pkgs[!pkgs %in% rownames(installed.packages())]
 if (length(to_install) > 0) {
   install.packages(to_install, repos = "https://cloud.r-project.org")
