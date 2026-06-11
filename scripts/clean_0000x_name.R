99c44afa3e0b24469821e1e89e39844498820bc0
Bjoern Siepe <bjoernsiepe@gmail.com>
2026-06-11 09:41:42 +0200

    refining cleaning template script
    more helper functions, simpler structure

--- scripts/clean_0000x_name.R
+++ scripts/clean_0000x_name.R
@@ -20,33 +20,28 @@ source(here("scripts", "functions_data.R"))
 
 
 #* Misc -------------------------------------------------------------------
+# convert string sentinels ("NA", "", ...) and NaN to real NA
+df <- recode_missing(df)
+
+
+# Read metadata -----------------------------------------------------------
+# loaded before checking so check_data() can cross-check data against metadata
+# Enter dataset ID here
+meta_data <- read_sheet(METADATA_URL)
+dataset_info <- meta_data |>
+  filter(dataset_id == "000X")
+variable_data <- read_sheet(pull(dataset_info, "Coding File URL"))
 
 
 # Check requirements ------------------------------------------------------
-# if check_data runs without messages, the data are clean
-# and should be saved as a .tsv file
-check_results <- check_data(df)
+# errors abort; warnings flag likely problems but still allow saving
+check_results <- check_data(df, dataset_info, variable_data)
 
 # if it returns "Data are clean.", save the data
-# Enter data set ID here
 if(check_results == "Data are clean."){
   write_tsv(df, here("data", "clean", "000X_NAME_ts.tsv"))
 }
 
 
 # Create metadata ---------------------------------------------------------
-metadata_url <- "https://docs.google.com/spreadsheets/d/1ALGCq_jN6I4dcjWYQ_LQe9o52DGJItwdu9fCkwOh6fg/edit?pli=1&gid=0#gid=0"
-meta_data <- read_sheet(metadata_url)
-
-
-# Enter dataset ID here
-sheet_url <- meta_data |>
-  filter(dataset_id == "000X") |>
-  pull("Coding File URL")
-
-variable_data <- read_sheet(sheet_url)
-
-meta_json <- create_metadata_json("000X") |>
-  toJSON(pretty = TRUE, auto_unbox = TRUE)
-
-write(meta_json, here("data", "metadata", "000X_NAME_metadata.json"))
+write_metadata("000X", meta_data = meta_data, variable_data = variable_data)
